0817d31df8e4b63d8f027c1f8c047ef323dfe81b
rafaabbade <82163300+rafaabbade@users.noreply.github.com>
2023-03-20 09:18:04 -0300

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -11,22 +11,22 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [2,3],
-            "answer": 5
+            "input": [[1, 2, 3, 4, 5]],
+            "answer": 9
         },
         {
-            "input": [-1,5],
-            "answer": 4
+            "input": [[2,4,6,8]],
+            "answer": 0
         },
         {
-            "input": [0,0],
+            "input": [[]],
             "answer": 0
         }
     ],
     "Extra": [
         {
-            "input": [-1,-1],
-            "answer": -2
+            "input": [[10,11,12,13,14,15],
+            "answer": 39
         },
     ]
 }
